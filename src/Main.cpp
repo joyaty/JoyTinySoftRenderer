5781f928088693b7ea9149c40c95349fdc153d59
joyaty <joyaty@gmail.com>
2025-02-17 16:56:00 +0800

    * vec2、vec3、vec4 template instantiation

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,17 +1,10 @@
 
+#include <cstdio>
 #include <iostream>
 #include "math/Vec.h"
 
 int main(int args_count, char** args)
 {
     std::cout << "Hello, JoyTinySoftRenderer!" << std::endl;
-
-    Joy::Vec<4> vec;
-    vec[0] = 3;
-    vec[1] = 4;
-    vec[2] = 0;
-    vec[3] = 0;
-
-    std::cout << vec.Norm() << ", " << vec.SqrNorm() << std::endl;
     return 0;
 }
--- src/math/Vec.h
+++ src/math/Vec.h
@@ -1,8 +1,9 @@
 /*
  * 简单实现的向量类模板
-*/
+ */
 
 #include <cmath>
+#include <cassert>
 
 namespace Joy
 {
@@ -13,37 +14,23 @@ namespace Joy
 	template<int N> class Vec
 	{
 	public:
-		/// <summary>
-		/// 获取向量指定维度值 - 只读
-		/// </summary>
-		/// <param name="index"></param>
-		/// <returns></returns>
 		const float& operator[](int index) const
 		{
+			assert(index >=0 && index < N);
 			return m_Data[index];
 		}
 		
-		/// <summary>
-		/// 获取向量指定维度值 - 可写入赋值
-		/// </summary>
 		float& operator[](int index)
 		{
+			assert(index >=0 && index < N);
 			return m_Data[index];
 		}
 
-		/// <summary>
-		/// 获取向量的模长
-		/// </summary>
-		/// <returns></returns>
 		float Norm() const
 		{
 			return sqrtf(SqrNorm());
 		}
 
-		/// <summary>
-		/// 获取向量的模长平方
-		/// </summary>
-		/// <returns></returns>
 		float SqrNorm() const
 		{
 			return (*this) * (*this);
@@ -53,13 +40,41 @@ namespace Joy
 		float m_Data[N] = { 0 };
 	};
 
-	/// <summary>
-	/// 重载两个向量*操作符
-	/// </summary>
-	/// <typeparam name="N"></typeparam>
-	/// <param name="vec1"></param>
-	/// <param name="vec2"></param>
-	/// <returns></returns>
+	template<int N> Vec<N> operator+ (const Vec<N>& vec1, const Vec<N>& vec2)
+	{
+		Vec<N> ret = vec1;
+		for(int i = 0; i < N; ++i)
+		{
+			ret[i] += vec2[i];
+		}
+		return ret;
+	}
+
+	template<int N> Vec<N> operator- (const Vec<N>& vec1, const Vec<N>& vec2)
+	{
+		Vec<N> ret = vec1;
+		for (int i = 0; i < N; ++i)
+		{
+			ret[i] -= vec2[i];
+		}
+		return ret;
+	}
+
+	template<int N> Vec<N> operator* (const Vec<N>& vec, float value)
+	{
+		Vec<N> ret = vec;
+		for (int i = 0; i < N; ++i) 
+		{
+			ret[i] *= value;
+		}
+		return ret;
+	}
+
+	template<int N> Vec<N> operator* (float value, const Vec<N>& vec)
+	{
+		return vec * value;
+	}
+
 	template<int N> float operator* (const Vec<N>& vec1, const Vec<N>& vec2)
 	{
 		float ret = 0;
@@ -69,4 +84,160 @@ namespace Joy
 		}
 		return ret;
 	}
+
+	template<int N> Vec<N> operator/ (const Vec<N>& vec, float value)
+	{
+		Vec<N> ret = vec;
+		for (int i = 0; i < N; ++i)
+		{
+			ret[i] /= value;
+		}
+		return ret;
+	}
+
+	template<int N> float Dot(const Vec<N>& vec1, const Vec<N>& vec2)
+	{
+		return vec1 * vec2;
+	}
+	
+	template<int N1, int N2> Vec<N1> Embed(const Vec<N2>& vec, float fill = 1)
+	{
+		Vec<N1> ret;
+		for(int i = 0; i < N1; ++i)
+		{
+			ret[i] = i < N2 ? vec[i] : fill;
+		}
+		return ret;
+	}
+
+	template<> class Vec<2> 
+	{
+	public:
+		const float& operator[] (int index) const
+		{
+			assert(index >=0 && index < 2);
+			return index == 0 ? x : y;
+		}
+
+		float& operator[] (int index)
+		{
+			assert(index >=0 && index < 2);
+			return index == 0 ? x : y;
+		}
+
+		float Norm() const
+		{
+			return sqrtf(SqrNorm());
+		}
+
+		float SqrNorm() const
+		{
+			return (*this) * (*this);
+		}
+
+		Vec<2> Normalize() const 
+		{
+			return (*this) / Norm();
+		}
+
+	public:
+		float x = 0, y = 0;
+	};
+
+	template<> class Vec<3>
+	{
+	public:
+		const float& operator[] (int index) const
+		{
+			assert(index >=0 && index < 3);
+			return index == 0 ? x : ((index == 1) ? y : z);
+		}
+
+		float& operator[] (int index)
+		{
+			assert(index >=0 && index < 3);
+			return index == 0 ? x : ((index == 1) ? y : z);
+		}
+
+		float Norm() const
+		{
+			return sqrtf(SqrNorm());
+		}
+
+		float SqrNorm() const
+		{
+			return (*this) * (*this);
+		}
+
+		Vec<3> Normalize() const
+		{
+			return (*this) / Norm();
+		}
+	
+	public:
+		float x = 0, y = 0, z = 0;
+	};
+
+	template<> class Vec<4>
+	{
+	public:
+		const float& operator[] (int index) const
+		{
+			assert(index >=0 && index < 4);
+			switch(index)
+			{
+			case 0:
+				return x;
+			case 1:
+				return y;
+			case 2:
+				return z;
+			case 3:
+			default:
+				return w;
+			}
+		}
+
+		float& operator[] (int index)
+		{
+			assert(index >=0 && index < 4);
+			switch(index)
+			{
+			case 0:
+				return x;
+			case 1:
+				return y;
+			case 2:
+				return z;
+			case 3:
+			default:
+				return w;
+			}
+		}
+
+		float Norm() const
+		{
+			return sqrtf(SqrNorm());
+		}
+
+		float SqrNorm() const
+		{
+			return (*this) * (*this);
+		}
+
+		Vec<4> Normalize() const
+		{
+			return (*this) / Norm();
+		}
+	
+	public:
+		float x = 0, y = 0, z = 0, w = 0;
+	};
+
+	typedef Vec<2> Vec2;
+	typedef Vec<3> Vec3;
+	typedef Vec<4> Vec4;
+	
+	float Cross(const Vec2& vec1, const Vec2& vec2);
+	Vec3 Cross(const Vec3& vec1, const Vec3& vec2);
 }
