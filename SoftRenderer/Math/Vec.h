7170a2479d9e2f6289f48d55a9850c1fc701669a
joyaty <joyaty@gmail.com>
2025-12-02 00:01:58 +0800

    add the operator for vec && add some testcase for the new operation.

--- SoftRenderer/Math/Vec.h
+++ SoftRenderer/Math/Vec.h
@@ -4,6 +4,8 @@
  */
 
 #include <cassert>
+#include <cmath>
+#include <ostream>
 
 namespace Joy
 {
@@ -18,6 +20,15 @@ namespace Joy
         static_assert(N > 0, "Vec dimension must be greater than 0.");
 
     public:
+        constexpr Vec()
+        {
+            for (int i = 0; i < N; ++i)
+            {
+                m_Data[i] = 0.f;
+            }
+        }
+
+    public:
         /**
          * @brief 获取N维向量指定维度的值 - 只读取版本
          *
@@ -26,7 +37,7 @@ namespace Joy
          */
         const T& operator[](const int index) const
         {
-            assert(index >= 0 && index < DIMENSIONS);
+            assert(index >= 0 && index < DIMENSION);
             return m_Data[index];
         }
 
@@ -38,7 +49,7 @@ namespace Joy
          */
         T& operator[](const int index)
         {
-            assert(index >= 0 && index < DIMENSIONS);
+            assert(index >= 0 && index < DIMENSION);
             return m_Data[index];
         }
 
@@ -50,12 +61,234 @@ namespace Joy
         T m_Data[N];
 
     public:
+        constexpr static Vec<N, T> Zero() { return Vec(); }
+
+    public:
         /**
          * @brief 向量维度层级
          *
          */
-        constexpr static int DIMENSIONS = N;
+        constexpr static int DIMENSION = N;
     };
+
+    /**
+     * @brief 判断两个向量是否相等
+     * 
+     * @tparam N 
+     * @tparam T 
+     * @param lhs 
+     * @param rhs 
+     * @return true 
+     * @return false 
+     */
+    template<int N, typename T> constexpr bool operator==(const Vec<N, T>& lhs, const Vec<N, T>& rhs)
+    {
+        for (int i = 0; i < N; ++i)
+        {
+            if (std::abs(lhs[i] - rhs[i]) >= 1e-5f)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * @brief 向量加法
+     *
+     * @tparam N
+     * @tparam T
+     * @param lhs
+     * @param rhs
+     * @return constexpr Vec<N, T>
+     */
+    template<int N, typename T> constexpr Vec<N, T> operator+(const Vec<N, T>& lhs, const Vec<N, T>& rhs)
+    {
+        Vec<N, T> ret = lhs;
+        for (int i = 0; i < N; ++i)
+        {
+            ret[i] += rhs[i];
+        }
+        return ret;
+    }
+
+    /**
+     * @brief 向量减法
+     *
+     * @tparam N
+     * @tparam T
+     * @param lhs
+     * @param rhs
+     * @return constexpr Vec<N, T>
+     */
+    template<int N, typename T> constexpr Vec<N, T> operator-(const Vec<N, T>& lhs, const Vec<N, T>& rhs)
+    {
+        Vec<N, T> ret = lhs;
+        for (int i = 0; i < N; ++i)
+        {
+            ret[i] -= rhs[i];
+        }
+        return ret;
+    }
+
+    /**
+     * @brief 分量乘法
+     *
+     * @tparam N
+     * @tparam T
+     * @param lhs
+     * @param rhs
+     * @return constexpr Vec<N, T>
+     */
+    template<int N, typename T> constexpr Vec<N, T> operator*(const Vec<N, T>& lhs, const Vec<N, T>& rhs)
+    {
+        Vec<N, T> ret = lhs;
+        for (int i = 0; i < N; ++i)
+        {
+            ret[i] *= rhs[i];
+        }
+        return ret;
+    }
+
+    /**
+     * @brief 标量后置乘法
+     *
+     * @tparam N
+     * @tparam T
+     * @param lhs
+     * @param scale
+     * @return constexpr Vec<N, T>
+     */
+    template<int N, typename T> constexpr Vec<N, T> operator*(const Vec<N, T>& lhs, const T& scale)
+    {
+        Vec<N, T> ret = lhs;
+        for (int i = 0; i < N; ++i)
+        {
+            ret[i] *= scale;
+        }
+        return ret;
+    }
+
+    /**
+     * @brief 标量前置乘法
+     *
+     * @tparam N
+     * @tparam T
+     * @param scale
+     * @param rhs
+     * @return constexpr Vec<N, T>
+     */
+    template<int N, typename T> constexpr Vec<N, T> operator*(const T& scale, const Vec<N, T>& rhs)
+    {
+        return rhs * scale;
+    }
+
+    /**
+     * @brief 后置标量除法
+     *
+     * @tparam N
+     * @tparam T
+     * @param lhs
+     * @param scale
+     * @return constexpr Vec<N, T>
+     */
+    template<int N, typename T> constexpr Vec<N, T> operator/(const Vec<N, T>& lhs, const T& scale)
+    {
+        assert(scale != 0.f);
+        Vec<N, T> ret = lhs;
+        for (int i = 0; i < N; ++i)
+        {
+            ret[i] /= scale;
+        }
+        return ret;
+    }
+
+    /**
+     * @brief 向量点积
+     *
+     * @tparam N
+     * @tparam T
+     * @param lhs
+     * @param rhs
+     * @return constexpr T
+     */
+    template<int N, typename T> constexpr T Dot(const Vec<N, T>& lhs, const Vec<N, T>& rhs)
+    {
+        T ret = 0;
+        for (int i = 0; i < N; ++i)
+        {
+            ret += lhs[i] * rhs[i];
+        }
+        return ret;
+    }
+
+    /**
+     * @brief 向量模长平方
+     *
+     * @tparam N
+     * @tparam T
+     * @param vec
+     * @return constexpr T
+     */
+    template<int N, typename T> constexpr T SqrNorm(const Vec<N, T>& vec)
+    {
+        return Dot(vec, vec);
+    }
+
+    /**
+     * @brief 获取向量模长
+     *
+     * @tparam N
+     * @tparam T
+     * @param vec
+     * @return constexpr T
+     */
+    template<int N, typename T> constexpr T Norm(const Vec<N, T>& vec)
+    {
+        return std::sqrt(SqrNorm(vec));
+    }
+
+    /**
+     * @brief 归一化向量
+     *
+     * @tparam N
+     * @tparam T
+     * @param vec
+     * @return constexpr Vec<N, T>
+     */
+    template<int N, typename T> constexpr Vec<N, T> Normalized(const Vec<N, T>& vec)
+    {
+        T norm = Norm(vec);
+        if (norm == 0.f)
+        {
+            return Vec<N, T>::Zero();
+        }
+        return vec / norm;
+    }
+
+    /**
+     * @brief 打印N维向量
+     *
+     * @tparam N
+     * @tparam T
+     * @param out
+     * @param vec
+     * @return std::ostream&
+     */
+    template<int N, typename T> std::ostream& operator<<(std::ostream& out, const Vec<N, T>& vec)
+    {
+        out << "(";
+        for (int i = 0; i < N; ++i)
+        {
+            out << vec[i];
+            if (i < N - 1)
+            {
+                out << ",";
+            }
+        }
+        out << ")";
+        return out;
+    }
 
     /**
      * @brief 2维浮点向量模板特化
@@ -73,12 +306,12 @@ namespace Joy
     public:
         const float& operator[](const int index) const
         {
-            assert(index >= 0 && index < DIMENSIONS);
+            assert(index >= 0 && index < DIMENSION);
             return index == 0 ? x : y;
         }
         float& operator[](const int index)
         {
-            assert(index >= 0 && index < DIMENSIONS);
+            assert(index >= 0 && index < DIMENSION);
             return index == 0 ? x : y;
         }
 
@@ -94,7 +327,7 @@ namespace Joy
         constexpr static Vec<2, float> Up() { return Vec<2, float>(0.f, 1.f); }
 
     public:
-        constexpr static int DIMENSIONS = 2;
+        constexpr static int DIMENSION = 2;
     };
 
     /**
@@ -114,12 +347,12 @@ namespace Joy
     public:
         const float& operator[](const int index) const
         {
-            assert(index >= 0 && index < DIMENSIONS);
+            assert(index >= 0 && index < DIMENSION);
             return index == 0 ? x : index == 1 ? y : z;
         }
         float& operator[](const int index)
         {
-            assert(index >= 0 && index < DIMENSIONS);
+            assert(index >= 0 && index < DIMENSION);
             return index == 0 ? x : index == 1 ? y : z;
         }
 
@@ -138,7 +371,7 @@ namespace Joy
         constexpr static Vec<3, float> Forward() { return Vec<3, float>(0.f, 0.f, 1.f); }
 
     public:
-        constexpr static int DIMENSIONS = 3;
+        constexpr static int DIMENSION = 3;
     };
 
     /**
@@ -159,12 +392,12 @@ namespace Joy
     public:
         const float& operator[](const int index) const
         {
-            assert(index >= 0 && index < DIMENSIONS);
+            assert(index >= 0 && index < DIMENSION);
             return index == 0 ? x : index == 1 ? y : index == 2 ? z : w;
         }
         float& operator[](const int index)
         {
-            assert(index >= 0 && index < DIMENSIONS);
+            assert(index >= 0 && index < DIMENSION);
             return index == 0 ? x : index == 1 ? y : index == 2 ? z : w;
         }
 
@@ -175,7 +408,7 @@ namespace Joy
         float w = 0.f;
 
     public:
-        constexpr static int DIMENSIONS = 4;
+        constexpr static int DIMENSION = 4;
     };
 
     /**
--- Tests/MathTest/VecTest.cpp
+++ Tests/MathTest/VecTest.cpp
@@ -1,14 +1,24 @@
 
+#include "Math/Vec.h"
 #include <gtest/gtest.h>
 
 namespace Joy
 {
     namespace UnitTest
     {
-        TEST(MathTest, VecTest)
+        TEST(MathTest, VecCommontTest)
         {
-            
+            Vec3f vec{Vec3f::Zero()};
+            EXPECT_EQ(SqrNorm(vec), 0.f);
+            EXPECT_EQ(Normalized(vec), Vec3f::Zero());
         }
-    }
+
+        TEST(MathTest, VecOperatorTest2)
+        {
+            Vec3f vec1{2.f, 1.f,  -7.f};
+            Vec3f vec2{0.f, -1.1f,  6.f};
+            EXPECT_EQ(vec1 + vec2, Vec3f(2.f, -0.1f, -1.f));
+        }
+    }   // namespace UnitTest
 
 }   // namespace Joy
